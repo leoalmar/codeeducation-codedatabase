8b98178cf1ee577ce0b96fcb9f60dfff73027919
Leonardo Almeida Araújo <leonardoxsystems@gmail.com>
2016-06-07 11:26:36 -0300

    Tests AbstractRepository

--- tests/CodeDatabase/AbstractRepositoryTest.php
+++ tests/CodeDatabase/AbstractRepositoryTest.php
@@ -87,7 +87,7 @@ class AbstractRepositoryTest extends AbstractTestCase
         $mockRepository = M::mock(AbstractRepository::class);
         $throw = new ModelNotFoundException();
         $throw->setModel(\stdClass::class);
-        
+
         $mockRepository
             ->shouldReceive('update')
             ->with(['name'=>'stdClassName'],0)
@@ -95,4 +95,120 @@ class AbstractRepositoryTest extends AbstractTestCase
 
         $mockRepository->update(['name'=>'stdClassName'],0);
     }
+
+    public function test_should_return_delete()
+    {
+        $mockRepository = M::mock(AbstractRepository::class);
+
+        $mockRepository
+            ->shouldReceive('delete')
+            ->with(1)
+            ->andReturn(true);
+
+        $result = $mockRepository->delete(1);
+        $this->assertEquals(true,$result);
+    }
+
+    /**
+     * @expectedException \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function test_should_return_delete_fail()
+    {
+        $mockRepository = M::mock(AbstractRepository::class);
+        $throw = new ModelNotFoundException();
+        $throw->setModel(\stdClass::class);
+
+        $mockRepository
+            ->shouldReceive('delete')
+            ->with(0)
+            ->andReturn($throw);
+
+        $mockRepository->delete(0);
+    }
+
+    public function test_should_return_find_without_columns_success()
+    {
+        $mockRepository = M::mock(AbstractRepository::class);
+
+        $mockStd = M::mock(\stdClass::class);
+        $mockStd->id = 1;
+        $mockStd->name = 'name';
+        $mockStd->description = 'description';
+        
+        $mockRepository
+            ->shouldReceive('find')
+            ->with(1)
+            ->andReturn($mockStd);
+        
+        $this->assertInstanceOf(\stdClass::class,$mockRepository->find(1));
+    }
+
+    public function test_should_return_find_with_columns_success()
+    {
+        $mockRepository = M::mock(AbstractRepository::class);
+
+        $mockStd = M::mock(\stdClass::class);
+        $mockStd->id = 1;
+        $mockStd->name = 'name';
+
+        $mockRepository
+            ->shouldReceive('find')
+            ->with(1,['id','name'])
+            ->andReturn($mockStd);
+
+        $this->assertInstanceOf(\stdClass::class,$mockRepository->find(1,['id','name']));
+    }
+
+    /**
+     * @expectedException \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function test_should_return_find_fail()
+    {
+        $mockRepository = M::mock(AbstractRepository::class);
+        $throw = new ModelNotFoundException();
+        $throw->setModel(\stdClass::class);
+
+        $mockRepository
+            ->shouldReceive('find')
+            ->with(0)
+            ->andReturn($throw);
+
+        $mockRepository->find(0);
+    }
+
+    public function test_should_return_findby_columns_success()
+    {
+        $mockRepository = M::mock(AbstractRepository::class);
+
+        $mockStd = M::mock(\stdClass::class);
+        $mockStd->id = 1;
+        $mockStd->name = 'name';
+
+        $mockRepository
+            ->shouldReceive('findBy')
+            ->with('name','my-data',['id','name'])
+            ->andReturn([$mockStd,$mockStd,$mockStd]);
+
+        $result = $mockRepository->findBy('name','my-data',['id','name']);
+        $this->assertCount(3,$result);
+        $this->assertInstanceOf(\stdClass::class,$result[0]);
+    }
+
+    public function test_should_return_findby_empty_success()
+    {
+        $mockRepository = M::mock(AbstractRepository::class);
+
+        $mockStd = M::mock(\stdClass::class);
+        $mockStd->id = 1;
+        $mockStd->name = 'name';
+
+        $mockRepository
+            ->shouldReceive('findBy')
+            ->with('name','',['id','name'])
+            ->andReturn([]);
+
+        $result = $mockRepository->findBy('name','',['id','name']);
+        $this->assertCount(0,$result);
+    }
+
 }
